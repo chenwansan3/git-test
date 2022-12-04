15f9dc731a3192753ac96ecd3346ac134a7999e2
chenwansan <C6874264wryx@outlook.com>
2022-12-04 15:01:43 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 陈万三
+ * @create 2022-12-04 14:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
